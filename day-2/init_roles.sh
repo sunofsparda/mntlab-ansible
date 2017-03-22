5221e75aa4c3683ec016cef4881c4496628b29dd
Sun <sunofsparda@gmail.com>
2017-03-23 02:00:05 +0300

    Update and rename init_role.sh to init_roles.sh

--- day-2/init_roles.sh
+++ day-2/init_roles.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
+touch site.yml
 mkdir -p roles/{java,java_test,tomcat,tomcat_test,nginx,nginx_test}/{defaults,vars,tasks,meta,handlers}
 touch roles/{java,java_test,tomcat,tomcat_test,nginx,nginx_test}/{defaults,vars,tasks,meta,handlers}/main.yml
