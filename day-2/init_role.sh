91deb19fa90c4579d12deebfddc90cee717f6571
Sun <sunofsparda@gmail.com>
2017-03-23 01:57:38 +0300

    day-2

---
+++ day-2/init_role.sh
@@ -0,0 +1,2 @@
+mkdir -p roles/{java,java_test,tomcat,tomcat_test,nginx,nginx_test}/{defaults,vars,tasks,meta,handlers}
+touch roles/{java,java_test,tomcat,tomcat_test,nginx,nginx_test}/{defaults,vars,tasks,meta,handlers}/main.yml
