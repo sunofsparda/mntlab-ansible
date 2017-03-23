65290a2167a81eaa91831c03844deca525741839
Anton Cherlyonok <sunofsparda@gmail.com>
2017-03-23 10:47:45 +0300

    day-2

--- day-2/init_roles.sh
+++ day-2/init_roles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 touch site.yml
-mkdir -p roles/{java,java_test,tomcat,tomcat_test,nginx,nginx_test}/{defaults,vars,tasks,meta,handlers}
+mkdir -p roles/{java,java_test,tomcat,tomcat_test,nginx,nginx_test}/{defaults,vars,tasks,meta,handlers,templates}
 touch roles/{java,java_test,tomcat,tomcat_test,nginx,nginx_test}/{defaults,vars,tasks,meta,handlers}/main.yml
