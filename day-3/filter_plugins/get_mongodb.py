32fa78db24337fece4a18e3e1872d2c525fb5925
Anton Cherlyonok <sunofsparda@gmail.com>
2017-03-26 20:12:30 +0300

    day-2 fixed mongodb filter

--- day-3/filter_plugins/get_mongodb.py
+++ day-3/filter_plugins/get_mongodb.py
@@ -1,20 +1,21 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
-
 from ansible import errors
+import re
 
-def get_mongodb(mongo_src,os_family,major_version,mongodb_version):
-    result=[]
-    dict={'RedHat':'rhel','Debian':'debian','Ubuntu':'ubuntu'}
-    for i in range(len(mongo_src)):
-        temp_src=mongo_src[i].split("-")
-        if (dict[os_family]+major_version) in temp_src[3]:
-            if mongodb_version in temp_src[4]:
-                result = mongo_src[i]
-    return result
+dict={'RedHat':'rhel','Debian':'debian','Ubuntu':'ubuntu'}
 
 class FilterModule(object):
+
     def filters(self):
         return {
-            'get_mongo_src': get_mongodb
+            'get_mongo_src': get_mongo_src,
         }
+
+def get_mongo_src(mongo_src, os_family, major_version, mongodb_version):    
+    result = []
+
+    for i in range(len(mongo_src)):
+        if ((dict[os_family] + major_version) in mongo_src[i]) and (str(mongodb_version) in mongo_src[i]):
+            result.append(mongo_src[i])
+    return "Recomended MongoDB versions for {} distro are: {}".format(os_family, result)
