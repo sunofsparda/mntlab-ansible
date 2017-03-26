43628bbd031e3d2fef9336d92b07394b80cc804c
Sun <sunofsparda@gmail.com>
2017-03-26 20:17:38 +0300

    day-3 fixed mongodb

--- day-3/filter_plugins/get_mongodb.py
+++ day-3/filter_plugins/get_mongodb.py
@@ -6,7 +6,6 @@ import re
 dict={'RedHat':'rhel','Debian':'debian','Ubuntu':'ubuntu'}
 
 class FilterModule(object):
-
     def filters(self):
         return {
             'get_mongo_src': get_mongo_src,
@@ -14,7 +13,6 @@ class FilterModule(object):
 
 def get_mongo_src(mongo_src, os_family, major_version, mongodb_version):    
     result = []
-
     for i in range(len(mongo_src)):
         if ((dict[os_family] + major_version) in mongo_src[i]) and (str(mongodb_version) in mongo_src[i]):
             result.append(mongo_src[i])
